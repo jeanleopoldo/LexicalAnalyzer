70e1537410f8b258b49812789ff2fe82a4d4c2a7
Jean Leopoldo <jean@jettacargo.com.br>
2022-05-31 09:50:31 -0300

    fix: getting reight previous lexeme

--- src/core/LexicalAnalyzer.py
+++ src/core/LexicalAnalyzer.py
@@ -1,14 +1,13 @@
-from .LexicalAnalyzerState import LexicalAnalyzerState
-
 class LexicalAnalyzer:
     def __init__(self, input, lexemes):
         self._input         = input
         self._lexemes       = lexemes
         self._found_lexemes = list()
+        self._queue         = list()
 
     def analyze(self):
         
-        queue          = list()
+        
         current_lexeme = self._lexemes["function"]
         self._found_lexemes.append(current_lexeme)
         current_state  = current_lexeme.initial_state()
@@ -18,46 +17,67 @@ class LexicalAnalyzer:
             if current_state.has_production_with_char(char):
                 next_state    = current_state.next_state(char)
                 current_state = current_lexeme.get_state(next_state)
+                current_lexeme.set_current_state(current_state)
             else:
-
                 if not current_state.is_final():
-                    lexical_state    = LexicalAnalyzerState(current_lexeme, current_state)
-                    queue.append(lexical_state)
+                    self._queue.append(current_lexeme)
                     possible_lexemes = current_state.get_productions_that_leads_to_another_lexeme()
-                    bfs              = list(possible_lexemes)
-                    current_lexeme   = self.find_next_lexeme(char, bfs)
-                    current_state    = current_lexeme.initial_state()
-                    next_state       = current_state.next_state(char)
-                    current_state    = current_lexeme.get_state(next_state)
+                    self.root(possible_lexemes, current_lexeme)
+                    current_lexeme = self.find_next_lexeme(char, possible_lexemes)
+
+                    current_state  = current_lexeme.get_current_state()
+                    next_state     = current_state.next_state(char)
+                    current_state  = current_lexeme.get_state(next_state)
+                    current_lexeme.set_current_state(current_state)
                 else:
-                    this_state       = queue.pop()
-                    current_lexeme   = this_state.lexeme()
-                    current_state    = this_state.state()
-                    next_state       = current_state.next_state(char)
-                    current_state    = current_lexeme.get_state(next_state)
-    
-    def find_next_lexeme(self, char, bfs):
+                    current_lexeme = self.get_previous_lexeme(current_lexeme)
+                    current_state  = current_lexeme.get_current_state()
+                    next_state     = current_state.next_state(char)
+                    current_state  = current_lexeme.get_state(next_state)
+                    current_lexeme.set_current_state(current_state)
+
+    def root(self, possible_lexemes, current_lexeme):
+        for lexeme_str in possible_lexemes:
+            lexeme = self.format(lexeme_str)
+            lexeme = self._lexemes[lexeme]
+            lexeme.set_generated_by(current_lexeme)
+
+    def find_next_lexeme(self, char, bfs): #TODO fix dis
         if len(bfs) == 0:
             raise RuntimeError("there are some errors in this language.")
-        possible_lexeme = bfs.pop(0)
-        possible_lexeme = possible_lexeme[1 : : ]
-        possible_lexeme = possible_lexeme[ : -1 : ]
+        raw             = bfs.pop(0)
+        possible_lexeme = self.format(raw)
         lexeme          = self._lexemes[possible_lexeme]
         if lexeme.initial_state().has_production_with_char(char):
             bfs.append(lexeme)
             self._found_lexemes.append(lexeme)
+            if not lexeme.get_generated_by() is None:
+                self._queue.append(lexeme)
             return lexeme
         
         possible_lexeme = self._lexemes[possible_lexeme]
-        lexemes = possible_lexeme.initial_state().get_productions_that_leads_to_another_lexeme()
-        for lexeme in lexemes:
-            possible_lexeme.generated_by(possible_lexeme)
-            bfs.append(lexeme)
+        lexemes         = possible_lexeme.initial_state().get_productions_that_leads_to_another_lexeme()
+        for lexeme_str in lexemes:
+            bfs.append(lexeme_str)
+            lexeme = self.format(lexeme_str)
+            lexeme = self._lexemes[lexeme]
+            lexeme.set_generated_by(possible_lexeme)
+            
         
         return self.find_next_lexeme(char, bfs)
            
+    def get_previous_lexeme(self, lexeme):
 
+        root = lexeme
+        while root.get_generated_by() is not None:
+            root     = root.get_generated_by()
+            if not root.get_current_state().is_final():
+                return root
+        return root
 
-                
+    def format(self, string):
+        string = string[1 : : ]
+        string = string[ : -1 : ]
+        return string
                 
     
--- src/model/Lexeme.py
+++ src/model/Lexeme.py
@@ -2,24 +2,32 @@ from .State import State
 
 class Lexeme:
     def __init__(self, json):
-        self._json = json
+        self._json         = json
+        self._generated_by = None
         self.create_states()
+        self._current_state = self._states["q0"]
 
     def create_states(self):
         self._states = {}
         for name in self._json["states"]:
-            json = self._json["states"][name]
-            state = State(name, json)
+            json               = self._json["states"][name]
+            state              = State(name, json)
             self._states[name] = state
-        
-        print("")
     
-    def generated_by(self, lexeme):
+    def set_generated_by(self, lexeme):
         self._generated_by = lexeme
-    
+
+    def get_generated_by(self):
+        return self._generated_by
+
     def name(self):
         return self._json["name"]
     
+    def set_current_state(self, state):
+        self._current_state = state
+
+    def get_current_state(self):
+        return self._current_state    
     def states(self):
         return self._states
     
