cadbf4d22039e2751ab743947533f458d0c01105
Jean Leopoldo <jean@jettacargo.com.br>
2022-06-02 10:38:29 -0300

    refact: created token model

--- src/model/Lexeme.py
+++ src/model/Lexeme.py
@@ -1,18 +1,8 @@
-from .State import State
 
-class Lexeme:
+from .Abstraction import Abstraction
+class Lexeme(Abstraction):
     def __init__(self, json):
-        self._json         = json
-        self._generated_by = None
-        self.create_states()
-        self._current_state = self._states["q0"]
-
-    def create_states(self):
-        self._states = {}
-        for name in self._json["states"]:
-            json               = self._json["states"][name]
-            state              = State(name, json)
-            self._states[name] = state
+        super().__init__(json)
     
     def set_generated_by(self, lexeme):
         self._generated_by = lexeme
@@ -20,22 +10,9 @@ class Lexeme:
     def get_generated_by(self):
         return self._generated_by
 
-    def name(self):
-        return self._json["name"]
-    
-    def set_current_state(self, state):
-        self._current_state = state
 
-    def get_current_state(self):
-        return self._current_state    
-    def states(self):
-        return self._states
     
-    def get_state(self, state):
-        return self._states[state]
-    
-    def initial_state(self):
-        return self._states["q0"]
+
     
 
     
---
+++ src/model/Token.py
@@ -0,0 +1,5 @@
+import json
+from .Abstraction import Abstraction
+class Token(Abstraction):
+    def __init__(self, json):
+        super().__init__(json)
