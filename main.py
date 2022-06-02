4584c3765ded6a656d20763dd07eff482473a1d9
Jean Leopoldo <jean@jettacargo.com.br>
2022-06-02 10:56:09 -0300

    refactor: minor refactor

--- main.py
+++ main.py
@@ -8,21 +8,24 @@ from src.core.LexicalAnalyzer             import LexicalAnalyzer
 
 _dirname = os.path.dirname(__file__)+"/"
 
+AVAILABLE_LEXEMES_AUTOMATA = _dirname+"syntaticAnalysis/automatons/lexemes.txt"
+LEXEMES_AUTOMATA_PATH      = _dirname+"syntaticAnalysis/automatons/json/"
+INPUT_PATH                 = _dirname+"expr/input.txt"
+TOKEN_PATH                 = _dirname+"lexicalAnalysis/tokens/"
+AVAILABLE_TOKENS_AUTOMATA  = _dirname+"lexicalAnalysis/automatons/tokens.txt"
+TOKENS_AUTOMATA_PATH       = _dirname+"lexicalAnalysis/automatons/json/"
+
 if __name__ == "__main__":
     
-    available_automatons  = _dirname+"syntaticAnalysis/automatons/lexemes.txt"
-    automatons_path       = _dirname+"syntaticAnalysis/automatons/json/"
-    input_path            = _dirname+"expr/input.txt"
-    tokens_path           = _dirname+"tokens/"
-    
-    lexemes_input_handler = LexemesInputHandler(automatons_path, available_automatons)
+    lexemes_input_handler = LexemesInputHandler(LEXEMES_AUTOMATA_PATH, AVAILABLE_LEXEMES_AUTOMATA)
     lexemes               = lexemes_input_handler.get_lexemes()
 
-    user_input_handler    = UserInputHandler(input_path)
+    user_input_handler    = UserInputHandler(INPUT_PATH)
     input                 = user_input_handler.get_input()
 
-    tokens_input_handler  = TokensInputHandler(tokens_path)
+    tokens_input_handler  = TokensInputHandler(TOKEN_PATH,AVAILABLE_TOKENS_AUTOMATA, TOKENS_AUTOMATA_PATH)
     tokens                = tokens_input_handler.get_tokens()
+    tokens_automatons     = tokens_input_handler.get_tokens_automatons()
 
     analyzer              = LexicalAnalyzer(input, tokens)
     analyzer.tokenize()
